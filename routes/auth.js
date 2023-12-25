d4234c7a984ffd4ed33833acc8f56382667140b2
basit-nazir-memon <basitnazir585@gmail.com>
2023-12-25 23:57:20 +0500

    backend Setup

---
+++ routes/auth.js
@@ -0,0 +1,144 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../middleware/auth');
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+router.post('/register', async (req, res) => {
+    const { fullName, username, email, password, confirm_password } = req.body;
+    
+
+
+    if (password != confirm_password){
+        return res.status(402).json({msg: 'Passwords not match'})
+    }
+
+    try {
+        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+        if (!password.match(passwordRegex)) {
+            return res.status(400).json({
+                msg: 'Password should have one lowercase letter, one uppercase letter, one special character, one number, and be at least 8 characters long',
+            });
+        }
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!email.match(emailRegex)) {
+            return res.status(400).json({ msg: 'Invalid email format' });
+        }
+
+        let existingUser = await User.findOne({ email });
+
+        if (existingUser) {
+            return res.status(400).json({ msg: 'Email is already in use' });
+        }
+
+        existingUser = await User.findOne({ username });
+        if (existingUser) {
+            return res.status(400).json({ msg: 'Username is already in use' });
+        }
+
+        let user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({ msg: 'User already exists' });
+        }
+        user = new User({
+            fullName,
+            username,
+            email,
+            password,
+        });
+
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        const payload = {
+            user: {
+                id: user.id,
+                role: 'user'
+            },
+        };
+
+        jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1h" }, (err, token) => {
+            if (err) throw err;
+            res.json({ token: token, id: user.id, role: 'user' });
+        });
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+router.post('/login', async (req, res) => {
+    const token = req.header('Authorization');
+    if (token) {
+        return res.status(401).json({ msg: 'Already Logged In' });
+    }
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({ msg: 'Invalid Credentials' });
+        }
+
+        if (user.blocked){
+            return res.status(400).json({msg: 'Account Blocked'});
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({ msg: 'Invalid Credentials' });
+        }
+
+        const payload = {
+            user: {
+                id: user.id,
+                role: user.role
+            },
+        };
+
+        jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: 3600 }, (err, token) => {
+            if (err) throw err;
+            res.json({ token, id: user.id, role: user.role });
+        });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+router.get('/profile', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+router.put('/profile', auth, async (req, res) => {
+    const { username, email } = req.body;
+
+    try {
+        let user = await User.findById(req.user.id);
+
+        user.username = username || user.username;
+        user.email = email || user.email;
+
+        await user.save();
+
+        res.json('Updated User');
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const nodemon = require('nodemon')
+const adminRoute = require('./routes/admin');
+const authRoute = require('./routes/auth');
+const blogPostRoute = require('./routes/blogPost');
+const searchRoute = require('./routes/search');
+const UserInteractionRoute = require('./routes/userInteraction');
+
+
+const cors = require('cors');
+require('dotenv').config()
+
+const app = express()
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.t1ompdc.mongodb.net/WebProject`, { useNewUrlParser: true })
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', ()=>{console.log("DB Connection Successfull")});
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use('/', authRoute);
+app.use('/', UserInteractionRoute);
+app.use('/admin', adminRoute);
+app.use('/posts', blogPostRoute);
+app.use('/search', searchRoute);
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(process.env.PORT, () => console.log(`App listening on port ${process.env.PORT}!`))
